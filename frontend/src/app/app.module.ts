4fd0cdaaa2a9b6352a856f9053ffa3544c98f0d7
Átila da Silva Lima <atilasilvalima@gmail.com>
2022-04-03 01:01:40 -0300

    Aulas Navegando entre Componentes, Diretivas Parte 01 e 02

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,13 +12,19 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
